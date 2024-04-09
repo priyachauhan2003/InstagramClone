47748c49d4bb24389efd2351aa05c6240516e65e
Priya Chauhan <priyachauhan.kkr@gmail.com>
2024-04-09 22:45:11 +0530

    first commit

---
+++ app/src/main/java/com/techmania/instagramclone/SignupActivity.kt
@@ -0,0 +1,85 @@
+package com.techmania.instagramclone
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Html
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.techmania.instagramclone.Models.user
+import com.techmania.instagramclone.databinding.ActivitySignupBinding
+import com.techmania.instagramclone.utils.USER_NODE
+import com.techmania.instagramclone.utils.USER_PROFILE_FOLDER
+import com.techmania.instagramclone.utils.uploadImage
+
+class SignupActivity : AppCompatActivity() {
+    val binding by lazy {
+        ActivitySignupBinding.inflate(layoutInflater)
+    }
+    lateinit var user:user
+    private val launcher=registerForActivityResult(ActivityResultContracts.GetContent()){
+        uri->
+        uri?.let{
+                uploadImage(uri, USER_PROFILE_FOLDER){
+                    if(it==null){
+
+                    }
+                    else{
+                        user.image=it
+                        binding.profileImage.setImageURI(uri)
+                    }
+                }
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val text="<font color=#FF000000>Already have an account? </font> <font color=#1E88E5>Login</font>"
+        binding.login.setText(Html.fromHtml(text))
+
+        user=user()
+        binding.signUpButton.setOnClickListener {
+            if(binding.name.editText?.text.toString().equals("") or
+                binding.email.editText?.text.toString().equals("") or
+                binding.password.editText?.text.toString().equals("")){
+                        Toast.makeText(this@SignupActivity,"Please fill the all information",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    binding.email.editText?.text.toString(),
+                    binding.password.editText?.text.toString()
+                ).addOnCompleteListener {
+                    result ->
+
+                    if(result.isSuccessful){
+                        user.name=binding.name.editText?.text.toString()
+                        user.email=binding.email.editText?.text.toString()
+                        user.password=binding.password.editText?.text.toString()
+                        Firebase.firestore.collection(USER_NODE)
+                            .document(Firebase.auth.currentUser!!.uid).set(user).
+                                addOnSuccessListener {
+                                    startActivity(Intent(this@SignupActivity,HomeActivity::class.java))
+                                    finish()
+                                }
+                    }
+                    else{
+                        Toast.makeText(this@SignupActivity,result.exception?.localizedMessage,Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+        binding.addImage.setOnClickListener {
+            launcher.launch("image/*")
+        }
+        binding.login.setOnClickListener {
+            startActivity(Intent(this@SignupActivity,LoginActivity::class.java))
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/techmania/instagramclone/utils/constants.kt
@@ -0,0 +1,6 @@
+package com.techmania.instagramclone.utils
+
+import com.google.firebase.firestore.auth.User
+
+const val USER_NODE="User"
+const val USER_PROFILE_FOLDER="Profile"
